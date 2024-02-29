e2a492d089395bc9a8585c56ab639762e5a30a16
hanomag <>
2024-02-29 11:16:08 +0300

    fix lots

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -47,7 +47,9 @@ document.addEventListener("DOMContentLoaded", () => {
       // Get the current page scroll position;
       const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
       const scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;
-
+      if (xl.matches) {
+        document.body.style.overflow = 'hidden'
+      }
       // if any scroll is attempted, set this to the previous value;
       window.onscroll = function () {
         window.scrollTo(scrollLeft, scrollTop);
@@ -55,6 +57,7 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     enableScroll() {
+      document.body.style.overflow = null
       window.onscroll = function () { };
     }
   }
